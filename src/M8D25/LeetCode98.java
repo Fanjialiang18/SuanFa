228cab228e24d8572d35e9868c1607f948a548a9
Clearlove3_lap <clearlovefan@gmail.com>
2021-08-25 20:11:33 +0800

    添加8月25的5题

---
+++ src/M8D25/LeetCode98.java
@@ -0,0 +1,26 @@
+package M8D25;
+
+import day01.TreeNode;
+
+/**
+ * 验证二叉搜索树
+ * 思路1:
+ *  递归
+ * 思路2:
+ *  中序遍历然后检查是否是升序
+ */
+public class LeetCode98 {
+    TreeNode pre;// 用来记录前一个节点
+    public boolean isValidBST(TreeNode root) {
+        if(root==null)
+            return true;
+        boolean left = isValidBST(root.left);
+        if(!left)
+            return false;
+        if(pre!=null&&root.val<=pre.val)
+            return false;
+        pre=root;
+        return isValidBST(root.right);
+    }
+
+}
