8996f9bce51b451c64adb04abdd0dbc36c489adc
clearlove3 <clearlovefan@gmail.com>
2021-09-28 22:32:19 +0800

    回溯

---
+++ src/huisu/LeetCode90.java
@@ -0,0 +1,38 @@
+package huisu;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author clearlove3
+ */
+public class LeetCode90 {
+    List<List<Integer>> result = new ArrayList<>();
+    LinkedList<Integer> path = new LinkedList<>();
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        if (nums.length == 0){
+            result.add(path);
+            return result;
+        }
+        Arrays.sort(nums);
+        subsetsWithDupHelper(nums, 0);
+        return result;
+    }
+
+    private void subsetsWithDupHelper(int[] nums, int startIndex){
+        result.add(new ArrayList<>(path));
+        if (startIndex >= nums.length){
+            return;
+        }
+        for (int i = startIndex; i < nums.length; i++){
+            if (i > startIndex && nums[i] == nums[i - 1]){
+                continue;
+            }
+            path.add(nums[i]);
+            subsetsWithDupHelper(nums, i + 1);
+            path.removeLast();
+        }
+    }
+}
