5166fd7121b25970a4533ae0fbef290036d6a0df
clearlove3 <clearlovefan@gmail.com>
2021-10-11 13:08:58 +0800

    dp开始

---
+++ src/dp/LeetCode70.java
@@ -0,0 +1,22 @@
+package dp;
+
+/**
+ * 爬楼梯
+ * @author clearlove3
+ */
+public class LeetCode70 {
+    public int climbStairs(int n) {
+        if(n<=2) {
+            return n;
+        }
+        int a=1;
+        int b=2;
+        int temp;
+        for(int i=3;i<=n;i++){
+            temp=a+b;
+            a=b;
+            b=temp;
+        }
+        return b;
+    }
+}
